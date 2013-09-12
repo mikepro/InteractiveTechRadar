7572ac22c391f3d6c9cbb46ab1e69f72efbe416e
=Michael <mike@somecomp.com>
2013-09-12 23:33:49 +0100

    Moving TrigFunctions to be a service rather than a factory

--- app/techradarapp/services/TrigFunctions.js
+++ app/techradarapp/services/TrigFunctions.js
@@ -1,14 +1,13 @@
 var app = angular.module('TechRadarApp');
-app.factory('TrigFunctions', function(){
-    return {
-        ConvertDegToRad: function(deg){
+app.service('TrigFunctions', function()
+    {
+        this.ConvertDegToRad = function(deg){
             return (deg * Math.PI)/180;
         },
-        ConvertRadToDeg: function(rad)
+        this.ConvertRadToDeg= function(rad)
         {
             return (rad * 180) / 180;
         }
-    };
-}
+    }
 );
 
