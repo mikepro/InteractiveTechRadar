a6f19c82b3a06ff852afd23d9ba7c20810fdc850
=Michael <mike@somecomp.com>
2013-09-18 21:57:33 +0100

    The group lines now grow and shrink when new rings are added

--- app/techradarapp/factories/TechRadarModel.js
+++ app/techradarapp/factories/TechRadarModel.js
@@ -5,6 +5,7 @@ app.factory('TechRadarModel',['BlipModel','RingModel',function(BlipModel,RingMod
         var self = this;
         self.rings = [];
         self.centerCords = {'x':centerX, 'y':centerY}
+        self.groupLines = [];
         self.init = function()
         {
             self.addRing(new RingModel('Adopt', 80, self.centerCords));
@@ -18,10 +19,31 @@ app.factory('TechRadarModel',['BlipModel','RingModel',function(BlipModel,RingMod
         self.addRing = function(ringModel)
         {
             self.rings.push(ringModel);
+            self.groupLines = computeGroupLines();
         }
         self.removeRing = function()
         {
             var removedRing = self.rings.pop();
+            self.groupLines = computeGroupLines();
+        }
+        function computeGroupLines()
+        {
+            var lastRingIndex = self.rings.length -1;
+            var bigestRadius = self.rings[lastRingIndex].radius;
+
+            var verticalx1 = self.centerCords.x;
+            var verticaly1 = self.centerCords.y -bigestRadius;
+            var verticalx2 = self.centerCords.x;
+            var verticaly2 = self.centerCords.y + bigestRadius;
+
+            var horzx1 = self.centerCords.x + bigestRadius
+            var horzy1 = self.centerCords.y;
+            var horzx2 = self.centerCords.x - bigestRadius;
+            var horzy2 = self.centerCords.y;
+            return [
+                {x1: horzx1, y1: horzy1, x2: horzx2, y2: horzy2},
+                {x1: verticalx1, y1: verticaly1, x2: verticalx2, y2: verticaly2}
+            ];
         }
     }
 }]);
