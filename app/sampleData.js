b5aaece059b2c9e37e47860cc1be92488402fa9d
michael <mike@somecomp.com>
2013-10-22 20:19:30 +0100

    Moving node web server out of the app directory

--- app/sampleData.js
+++ app/sampleData.js
@@ -1,24 +1,62 @@
 {
-    "numberOfGroups": 4, 
-    "rings": [
-        {"title":"Adopt", "blips":[
-                {"id":"1","name":"Cool tech","category":"","isNew": false,"group":0},
-                {"id":"2","name":"Amazing tech","category":"","isNew": true,"group":2},
-                {"id":"3","name":"okay tech","category":"","isNew": false,"group":2},
-                {"id":"4","name":"Angular js","category":"","isNew": true,"group":0}
-            ]},
-        {"title":"Trial", "blips":[
-                {"id":"5","name":"Tech 1","category":"","isNew": false,"group":0},
-                {"id":"6","name":"Tech 2","category":"","isNew": true,"group":0},
-                {"id":"7","name":"Tech 3","category":"","isNew": false,"group":1},
-                {"id":"8","name":"Tech 4","category":"","isNew": true,"group":1},
-                {"id":"9","name":"Tech 5","category":"","isNew": false,"group":0},
-                {"id":"10","name":"Tech 6","category":"","isNew": false,"group":0}
+"rings": [
+    {"title":"Adopt", "blips":[
+            {"id":"1", "isNew": true, "group": 0, "name": "AngularJS"},
+            {"id":"2", "isNew": true, "group": 0, "name": "Knockout"},
+            {"id":"3", "isNew": true, "group": 0, "name": "SASS"},
+            {"id":"4", "isNew": true, "group": 0, "name": "Bootstrap"},
+            {"id":"5", "isNew": true, "group": 0, "name": "SPA"},
+            {"id":"6", "isNew": true, "group": 0, "name": "Jasmine"},
+            {"id":"7", "isNew": true, "group": 0, "name": "Selenium Web Driver"},
+            {"id":"8", "isNew": true, "group": 0, "name": "Jquery"},
+            {"id":"9", "isNew": true, "group": 0, "name": "HTML"},
+            {"id":"10", "isNew": true, "group": 0, "name": "CSS"},
+
+            {"id":"11", "isNew": true, "group":1, "name":"Rabbit"},
+            {"id":"12", "isNew": true, "group":1, "name":"Mongo"},
+            {"id":"13", "isNew": true, "group":1, "name":"Continuous Integration"},
+            {"id":"14", "isNew": true, "group":1, "name":"Git"},
+            {"id":"15", "isNew": true, "group":1, "name":"NUnit"},
+            {"id":"16", "isNew": true, "group":1, "name":"RhinoMocks"},
+            {"id":"17", "isNew": true, "group":1, "name":"Vagrant"},
+            {"id":"18", "isNew": true, "group":1, "name":"Virtual box"},
+            {"id":"19", "isNew": true, "group":1, "name":"Jenkins"},
+            {"id":"20", "isNew": true, "group":1, "name":"Continuous Delivery"},
+            {"id":"21", "isNew": true, "group":1, "name":"Splunk"},
+            {"id":"21", "isNew": true, "group":1, "name":"Configuration Managment"},
+            {"id":"22", "isNew": true, "group":1, "name":"Resharper"},
+            {"id":"23", "isNew": true, "group":1, "name":"Vim :-)"},
+
+            {"id":"24", "isNew": true, "group":2, "name":"Feature Toggles"},
+            {"id":"25", "isNew": true, "group":2, "name":"Ioslated components"},
+            {"id":"26", "isNew": true, "group":2, "name":"Inversion of control"},
+            {"id":"27", "isNew": true, "group":2, "name":"Poloygot programers"},
+
+            {"id":"28", "isNew": true,"group":3, "name":"C#"},
+            {"id":"29", "isNew": true,"group":3, "name":"Javascript"},
+            {"id":"30", "isNew": true,"group":3, "name":"Ruby"}
         ]},
-        {"title":"Candidate", "blips":[
-                {"id":"11","name":"Tech 7","category":"","isNew": true,"group":1},
-                {"id":"12","name":"Tech 8","category":"","isNew": false,"group":2},
-                {"id":"13","name":"Tech 9","category":"","isNew": true,"group":3}
+    {"title":"Trial", "blips":[
+        {"id": "31","isNew":true,"group":0,"name":"Plasma"},
+        {"id": "32","isNew":true,"group":0,"name":"Web storage"},
+        {"id": "33","isNew":true,"group":0,"name":"JqueryUI"},
+        {"id": "34","isNew":true,"group":1,"name":"Node"},
+        {"id": "35","isNew":true,"group":1,"name":"Mongose"},
+        {"id": "36","isNew":true,"group":1,"name":"NPM"},
+        {"id": "37","isNew":true,"group":2,"name":"Big visible metrics"},
+        {"id": "38","isNew":true,"group":2,"name":"Naztag bunnies"},
+        {"id": "39","isNew":true,"group":2,"name":"Build monitors"},
+        {"id": "40","isNew":true,"group":2,"name":"Custom dashboards"},
+        {"id": "41","isNew":true,"group":2,"name":"Information radiators"}
+    ]},
+    {"title":"Candidate", "blips":[
+        {"id":"42","isNew":true,"group":0,"name":"Foundation"},
+        {"id":"43","isNew":true,"group":0,"name":"Font Awesome"},
+        {"id":"45","isNew":true,"group":0,"name":"CSS3"},
+        {"id":"46","isNew":true,"group":3,"name":"CoffeScript"},
+        {"id":"47","isNew":true,"group":3,"name":"F#"},
+        {"id":"48","isNew":true,"group":3,"name":"Lisp"},
+        {"id":"49","isNew":true,"group":3,"name":"Go"}
         ]}
-    ]
+]
 }
